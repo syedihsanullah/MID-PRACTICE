3783453fd44ba2628251674f0c4d544e8662fbe7
syedihsanullah <syedahsanullah4@gmail.com>
2024-11-29 18:33:43 +0500

    first commit

---
+++ Ihsan lab task 01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int c[4][4]={{7,8,9,5},{3,5,6,4},{7,8,9,5},{3,5,6,4}};
+    for(int d=0;d<4;d++){
+        for(int e=0;e<4;e++){
+            cout<<c[d][e]<<"\t";
+        }
+    
+    
+        cout<<"\n";
+    
+    }
+    
+}
---
+++ Ihsan lab task 02.cpp
@@ -0,0 +1,19 @@
+include <iostream>
+using namespace std;
+int main(){
+    int c[3][4];
+    for(int d=0;d<3;d++){
+        for(int e=0;e<4;e++){
+            cin>>c[d][e];
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout<<c[d][e];
+        }
+         cout<<"\n";
+    }
+    
+}
